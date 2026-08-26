307e5b40b6a68a31e2062c7499567de7b7c8f735
Paris Amorita Nyoni <nyoniparis37@gmail.com>
2026-08-26 16:11:25 +0000

    Added new feature :gbif_api file

--- gbif_API.py
+++ gbif_API.py
@@ -1,12 +1,41 @@
 import requests
+import json
 
+# print(response.url)
+# print(response.status_code)
+# print(len(records))
+
+all_records=[]
+isrequesting_pages=True
 url="https://api.gbif.org/v1/occurrence/search"
 
 params={"country":"ZA",
         "basisOfRecord":"FOSSIL_SPECIMEN",
-        "limit":20}
+        "limit":20 ,
+        "offset":0
+        }
 
+
+params["offset"]=0
+params["limit"]=300
 response=requests.get(url,params=params)
+data=response.json()
+record=data["results"]
+while isrequesting_pages:
 
-print(response.url)
-print(response.status_code)
+    record=data["results"][0]
+    response=requests.get(url,params=params)
+    data=response.json()
+    all_records.extend(data["results"])
+
+    if data["endOfRecords"]==True:
+        isrequesting_pages=data["endOfRecords"]
+        print("All the records have been accessed...")
+    else:
+       params["offset"] += params["limit"]
+    print(data["results"][0]["occurrenceID"])
+    print(record.get("year"))
+    print(data["results"][0]["basisOfRecord"])
+    print((data["results"][0]).get("decimalLatitude"))
+
+   
